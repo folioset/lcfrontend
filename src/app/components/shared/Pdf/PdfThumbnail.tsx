8b6b0a24ec87eb5f06ff086827ada694a047462a
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2021-07-08 21:45:09 +0530

    fixed pdf worker

--- src/app/components/shared/Pdf/PdfThumbnail.tsx
+++ src/app/components/shared/Pdf/PdfThumbnail.tsx
@@ -1,11 +1,16 @@
 import { Box } from '@material-ui/core';
 import * as React from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { pdfjs } from 'react-pdf';
 
 const PdfThumbnail: React.FC<{ file: string; onClick: (e: any) => void }> = ({
 	file,
 	onClick,
 }) => {
+	React.useEffect(() => {
+		pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+	}, []);
+
 	return (
 		<Box style={{ cursor: 'pointer' }} onClick={onClick}>
 			<Document file={file}>
